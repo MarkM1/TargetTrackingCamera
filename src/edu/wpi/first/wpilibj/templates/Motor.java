5ac00bcb185f9211a1e6d9754fac1cc415227593
Mark Macerato <15mmacerato@fatherjudgestudent.com>
2014-03-28 11:20:46 -0400

    Update Motor.java

--- src/edu/wpi/first/wpilibj/templates/Motor.java
+++ src/edu/wpi/first/wpilibj/templates/Motor.java
@@ -11,27 +11,33 @@ import edu.wpi.first.wpilibj.Servo;
  */
 class Motor {
 
-    private double acceptableRange;  //The distance to the left or right of the target in inches that is acceptable
+    private double acceptableRange = 0.05;  //The distance to the left or right of the target in inches that is acceptable
     private Servo motor = new Servo(1, 1);  //The Servo that controls the rotating motor that aligns with the target
+    private TargetLocator targetLocator = new TargetLocator(); //Will locate the target
 
     public void trackTheTarget() {  //Rotates the motor aligning with the target.  If the camera is too far to the left, it roates the motor right, and vice versa
 
-        if (TargetLocator.getRelPositionOfTarget() > acceptableRange) {  //if the target is too far to the right
+        if (targetLocator.findTarget() > acceptableRange) {  //if the target is too far to the right
 
             motor.set(1.0);
-            dirverStation.print("Turning right...");
+            dirverStation.print("Turning right...", 1);
 
-        } else if (TargetLocator.getRelPositionOfTarget() < -acceptableRange) {  //if the target is too far to the left
+        } else if (targetLocator.findTarget() < -acceptableRange) {  //if the target is too far to the left
 
             motor.set(0.0);
-            dirverStation.print("Turning left...");
+            dirverStation.print("Turning left...", 1);
             
-        } else if (TargetLocator.getRelPositionOfTarget() < acceptableRange && TargetLocator.getRelPositionOfTarget() > -acceptableRange) {  //If the camera is aligned
+        } else if (targetLocator.findTarget() < acceptableRange && targetLocator.findTarget() > -acceptableRange) {  //If the camera is aligned
 
             motor.set(motor.get());
-            dirverStation.print("Aligned!");
+            dirverStation.print("Aligned!", 1);
+
+        } else if(targetLocator.findTarget() == 0.0) { //If nothing is found
+            
+            dirverStation.print("No target", 1);
 
         }
+        
 
     }
 }
